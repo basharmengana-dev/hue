47906bdd97f4952e1eebbb6d0d441b53e4f8f6b8
Bashar Mengana <mengana@kth.se>
2024-03-24 15:34:09 +0000

    remove all comments, working state

--- src/Hue.tsx
+++ src/Hue.tsx
@@ -152,15 +152,14 @@ const createStreamedGrid = ({
     height,
   })
 
-  return baseGrid.slice(4, 5).map(({ x, y }, i) => {
-    // console.log('Stream for: ', 'x: ', x, 'y: ', y)
+  // .slice(4, 5)
+  return baseGrid.map(({ x, y }, i) => {
     return Array.from({ length: totalColumns - 1 }, (_, i) => {
       const p = {
         x: x + hSize * (i + 2 - totalColumns),
         y: y,
         color: colorStream[0],
       }
-      // console.log(p.x, ', ', p.y)
 
       return p
     })
@@ -220,12 +219,6 @@ export const Hue: React.FC = () => {
       }),
     [],
   )
-
-  // const totalPoints = gridStreams.length * gridStreams[0].length
-  // console.log('Total points: ', totalPoints)
-  // console.log('points per page', (cols + 1) * (rows + 1))
-  // console.log('total points', (cols + 1) * (rows + 1) * (pages + 1))
-  // console.log('cols', cols, 'rows', rows, 'pages', pages)
 
   const currentStep = useSharedValue(0)
   const direction = useSharedValue(0)
@@ -292,32 +285,25 @@ export const Hue: React.FC = () => {
     points: number[],
   ): number {
     'worklet'
-    // Ensure the points array has exactly three points
+
     if (points.length !== 3) {
       return points[0]
     }
 
-    // Extract the start, mid, and end positions from the points array
-    // When t is negative, reverse the points order to simulate rolling back
     const [startPosition, midPosition, endPosition] =
       t <= 0 ? points : [...points].reverse()
 
-    // Use the absolute value of t for calculation
     const absT = Math.abs(t)
 
-    // Calculate the x position based on the progress value t
     let xPosition: number
 
-    if (absT >= 0 && absT <= 0.5) {
-      // Interpolate between startPosition and midPosition
+    if (absT > 0 && absT <= 0.5) {
       xPosition = startPosition + (midPosition - startPosition) * (absT / 0.5)
-    } else if (absT > 0.5 && absT <= 1) {
-      // Interpolate between midPosition and endPosition
+    } else if (absT > 0.5 && absT < 1) {
       xPosition =
         midPosition + (endPosition - midPosition) * ((absT - 0.5) / 0.5)
     } else {
-      // Out of bounds, return the start or end position as a fallback
-      xPosition = absT <= 0 ? startPosition : endPosition
+      xPosition = direction.value < 0 ? startPosition : endPosition
     }
 
     return xPosition
@@ -325,11 +311,23 @@ export const Hue: React.FC = () => {
 
   const findMoveToClosestT = (x: number, points: number[]): number => {
     'worklet'
-    if (x >= points[0] + (points[2] - points[0]) * 0.4) {
-      return currentStep.value
-    } else {
-      return direction.value * 2
+    let closestT = currentStep.value
+
+    if (direction.value === -1) {
+      if (x >= points[0] + (points[2] - points[0]) * 0.4) {
+        closestT = currentStep.value
+      } else {
+        closestT = direction.value * 2
+      }
+    } else if (direction.value === 1) {
+      if (x <= points[0] - (points[0] - points[2]) * 0.4) {
+        closestT = direction.value * 2
+      } else {
+        closestT = currentStep.value
+      }
     }
+
+    return closestT
   }
 
   const moveToClosestTValue = useSharedValue(currentStep.value)
@@ -337,12 +335,6 @@ export const Hue: React.FC = () => {
     () => offset.value,
     (current, _) => {
       target.forEach((vertex, i) => {
-        // vertex.x.value =
-        //   currentGrid.value[i].x +
-        //   direction.value *
-        //     (targetGrid.value[i].x - currentGrid.value[i].x) *
-        //     current
-
         let startSlice = 0
         let endSlice = 0
         if (direction.value === -1) {
@@ -353,18 +345,15 @@ export const Hue: React.FC = () => {
           endSlice = gridStreams[i].length + currentStep.value + 2
         }
 
-        console.log('startSlice: ', startSlice, 'endSlice: ', endSlice)
-
         const last3 = gridStreams[i]
           .slice(startSlice, endSlice)
           .map(p => p.x)
           .reverse()
-        console.log('last3: ', last3)
+
         vertex.x.value = calculateDynamicXPositionWithDirection(current, last3)
         vertex.y.value = currentGrid.value[i].y
 
         moveToClosestTValue.value = findMoveToClosestT(vertex.x.value, last3)
-        // console.log('moveToCloestTValue: ', moveToClosestTValue.value)
 
         if (current !== 0) {
           const color_ = interpolateColor(
@@ -379,29 +368,20 @@ export const Hue: React.FC = () => {
     },
   )
 
-  // useAnimatedReaction(
-  //   () => currentStep.value,
-  //   current => {
-  //     console.log('Current step: ', current)
-  //   },
-  // )
-
   useAnimatedReaction(
     () => isMouseDownForPanning.value,
     (current, previous) => {
       if (current === false && previous === true) {
         if (isPanningRunning.value === true && direction.value !== 0) {
           if (Math.abs(offset.value) > autoScrollThreshold) {
-            console.log('moveToClosestTValue: ', moveToClosestTValue.value)
-            // console.log(' direction: ', direction.value)
             currentStep.value = currentStep.value + moveToClosestTValue.value
-            moveToClosestTValue.value = 0
           } else {
             currentGridAtTargetReady.value = true
           }
 
           offset.value = 0
           isPanningRunning.value = false
+          moveToClosestTValue.value = 0
         }
       }
     },
@@ -448,7 +428,6 @@ export const Hue: React.FC = () => {
       if (isPanningRunning.value == false) {
         return
       } else {
-        // console.log(event.translationX, width)
         offset.value = (event.translationX / width) * 1
       }
     })
